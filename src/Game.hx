05bd3ea37e8bd4bfbde7a8ddfb88623054fb6c40
ddmills <mills.dalton@gmail.com>
2024-02-14 12:39:03 -0600

    mouse and keyboard input

--- src/Game.hx
+++ src/Game.hx
@@ -1,3 +1,7 @@
+import kha.Scheduler;
+import kha.input.KeyCode;
+import kha.input.Keyboard;
+import kha.input.Mouse;
 import kha.Image;
 import kha.Assets;
 import kha.graphics4.TextureUnit;
@@ -73,6 +77,28 @@ class Game
 	var mvpId:ConstantLocation;
 	var textureId:TextureUnit;
 	var image:Image;
+	
+	var speed = 3.0; // 3 units / second
+	var mouseSpeed = 0.005;
+
+	var position:FastVector3 = new FastVector3(0, 0, 5); // Initial position: on +Z
+	var horizontalAngle = 3.14; // Initial horizontal angle: toward -Z
+	var verticalAngle = 0.0; // Initial vertical angle: none
+
+	var isMouseDown:Bool;
+	var mouseDeltaX:Int;
+	var mouseDeltaY:Int;
+	var mouseX:Int;
+	var mouseY:Int;
+
+	var moveForward:Bool;
+	var moveBackward:Bool;
+	var strafeLeft:Bool;
+	var strafeRight:Bool;
+
+	var lastTime:Float;
+
+	var projection:FastMatrix4;
 
 	public function new()
 	{
@@ -81,6 +107,9 @@ class Game
 		structure.add("col", VertexData.Float3);
 		structure.add("uv", VertexData.Float2);
 		var structureLength = 8;
+
+		Mouse.get().notify(onMouseDown, onMouseUp, onMouseMove, null);
+		Keyboard.get().notify(onKeyDown, onKeyUp);
 
 		pipeline = new PipelineState();
 		pipeline.inputLayout = [structure];
@@ -101,7 +130,7 @@ class Game
 		// trace(px.length);
 
 		// Projection matrix: 45° Field of View, 4:3 ratio, 0.1-100 display range
-		var projection = FastMatrix4.perspectiveProjection(45.0, 4.0 / 3.0, 0.1, 100.0);
+		projection = FastMatrix4.perspectiveProjection(45.0, 4.0 / 3.0, 0.1, 100.0);
 
 		// Camera matrix
 		var view = FastMatrix4.lookAt(new FastVector3(3, 2, 2), // position in world space
@@ -149,7 +178,63 @@ class Game
 		ib.unlock();
 	}
 
-	public function update() {}
+	public function update() {
+		var deltaTime = Scheduler.time() - lastTime;
+		lastTime = Scheduler.time();
+
+		if (isMouseDown) {
+			horizontalAngle += mouseSpeed * mouseDeltaX * -1;
+			verticalAngle += mouseSpeed * mouseDeltaY * -1;
+		}
+
+		var direction = new FastVector3(
+			Math.cos(verticalAngle) * Math.sin(horizontalAngle),
+			Math.sin(verticalAngle),
+			Math.cos(verticalAngle) * Math.cos(horizontalAngle)
+		);
+
+		var right = new FastVector3(
+			Math.sin(horizontalAngle - 3.13 / 2.0),
+			0,
+			Math.cos(horizontalAngle - 3.14 / 2.0)
+		);
+
+		var up = right.cross(direction);
+
+		if (moveForward) {
+			var v = direction.mult(deltaTime * speed);
+			position = position.add(v);
+		}
+		if (moveBackward) {
+			var v = direction.mult(deltaTime * speed * -1);
+			position = position.add(v);
+		}
+		if (strafeRight) {
+			var v = right.mult(deltaTime * speed);
+			position = position.add(v);
+		}
+		if (strafeLeft) {
+			var v = right.mult(deltaTime * speed * -1);
+			position = position.add(v);
+		}
+
+		var look = position.add(direction);
+
+		var view = FastMatrix4.lookAt(
+			position,
+			look,
+			up
+		);
+		var model = FastMatrix4.identity();
+
+		mvp = FastMatrix4.identity();
+		mvp = mvp.multmat(projection);
+		mvp = mvp.multmat(view);
+		mvp = mvp.multmat(model);
+
+		mouseDeltaX = 0;
+		mouseDeltaY = 0;
+	}
 
 	public function render(frames:Array<Framebuffer>)
 	{
@@ -169,5 +254,49 @@ class Game
 		g.drawIndexedVertices();
 
 		g.end();
+	}
+
+	function onMouseDown(button:Int, x:Int, y:Int)
+	{
+		isMouseDown = true;
+	}
+
+	function onMouseUp(button:Int, x:Int, y:Int)
+	{
+		isMouseDown = false;
+	}
+
+	function onMouseMove(x:Int, y:Int, movementX:Int, movementY:Int)
+	{
+		mouseDeltaX = x - mouseX;
+		mouseDeltaY = y - mouseY;
+
+		mouseX = x;
+		mouseY = y;
+	}
+
+	function onKeyDown(key:KeyCode)
+	{
+		trace('key down', key);
+		if (key == KeyCode.Up)
+			moveForward = true;
+		else if (key == KeyCode.Down)
+			moveBackward = true;
+		else if (key == KeyCode.Left)
+			strafeLeft = true;
+		else if (key == KeyCode.Right)
+			strafeRight = true;
+	}
+
+	function onKeyUp(key:KeyCode)
+	{
+		if (key == KeyCode.Up)
+			moveForward = false;
+		else if (key == KeyCode.Down)
+			moveBackward = false;
+		else if (key == KeyCode.Left)
+			strafeLeft = false;
+		else if (key == KeyCode.Right)
+			strafeRight = false;
 	}
 }
