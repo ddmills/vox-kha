59f49579e3a9e43ba5ca2c869d707bca5b8ffa22
ddmills <mills.dalton@gmail.com>
2024-02-16 17:56:38 -0600

    move program pipeline to mesh

--- src/vox/Game.hx
+++ src/vox/Game.hx
@@ -23,11 +23,7 @@ import kha.Framebuffer;
 
 class Game
 {
-	var pipeline:PipelineState;
 	var vp:FastMatrix4;
-	var mvpId:ConstantLocation;
-	var textureId:TextureUnit;
-	var image:Image;
 	
 	var speed = 3.0; // 3 units / second
 	var mouseSpeed = 0.005;
@@ -56,7 +52,6 @@ class Game
 	var projection:FastMatrix4;
 
 	var scene:Object;
-	var camera:Object;
 	var cube1:Object;
 
 	private function prettyMatrix(m:FastMatrix4) : String
@@ -71,17 +66,18 @@ class Game
 	public function new()
 	{
 		// create cube mesh
-		var structure = new VertexStructure();
-		structure.add("pos", VertexData.Float3);
-		structure.add("uv", VertexData.Float2);
-		
 		var blob:Blob = Assets.blobs.cube_obj;
 		var obj = new ObjLoader(blob);
 		var mesh = new Mesh();
 		mesh.vertices = obj.data;
 		mesh.indices = obj.indices;
-		mesh.structure = structure;
-		mesh.updateBuffers();
+		mesh.structure = new VertexStructure();
+		mesh.structure.add("pos", VertexData.Float3);
+		mesh.structure.add("uv", VertexData.Float2);
+		mesh.vertexShader = Shaders.simple_vert;
+		mesh.fragementShader = Shaders.simple_frag;
+		mesh.image = Assets.images.uvmap;
+		mesh.compile();
 
 		scene = new Object('scene');
 		cube1 = new Object('cube1', scene);
@@ -105,26 +101,8 @@ class Game
 		Mouse.get().notify(onMouseDown, onMouseUp, onMouseMove, null);
 		Keyboard.get().notify(onKeyDown, onKeyUp);
 
-		pipeline = new PipelineState();
-		pipeline.inputLayout = [structure];
-		pipeline.fragmentShader = Shaders.simple_frag;
-		pipeline.vertexShader = Shaders.simple_vert;
-		pipeline.depthWrite = true;
-		pipeline.depthMode = CompareMode.Less;
-		// pipeline.cullMode = CullMode.Clockwise; // hide interior hidden faces. Important that verticies are in the right order
-		// pipeline.colorAttachmentCount = 1;
-		// pipeline.colorAttachments[0] = TextureFormat.RGBA32;
-		// pipeline.depthStencilAttachment = DepthStencilFormat.Depth16;
-		pipeline.compile();
-
-		mvpId = pipeline.getConstantLocation("MVP");
-		textureId = pipeline.getTextureUnit("myTextureSampler");
-
-		image = Assets.images.uvmap;
-
 		// Projection matrix: 45° Field of View, 4:3 ratio, 0.1-100 display range
 		projection = FastMatrix4.perspectiveProjection(45.0, 4.0 / 3.0, 0.1, 100.0);
-
 		vp = FastMatrix4.identity();
 	}
 
@@ -194,9 +172,7 @@ class Game
 		var g = fb.g4;
 
 		g.begin();
-		g.setPipeline(pipeline);
 		g.clear(Color.fromFloats(0.0, 0.0, 0.3), 1);
-		g.setTexture(textureId, image);
 
 		renderObject(g, scene);
 
@@ -207,10 +183,7 @@ class Game
 	{
 		if (o.mesh != null) {
 			var mvp = vp.multmat(o.getWorldTransformationMatrix());
-			g.setMatrix(mvpId, mvp);
-			g.setVertexBuffer(o.mesh.vertexBuffer);
-			g.setIndexBuffer(o.mesh.indexBuffer);
-			g.drawIndexedVertices();
+			o.mesh.draw(g, mvp);
 		}
 
 		for (c in o.children)
--- src/vox/Mesh.hx
+++ src/vox/Mesh.hx
@@ -1,5 +1,14 @@
 package vox;
 
+import kha.graphics4.Graphics;
+import kha.graphics4.ConstantLocation;
+import kha.Image;
+import kha.graphics4.TextureUnit;
+import kha.math.FastMatrix4;
+import kha.graphics4.CompareMode;
+import kha.graphics4.FragmentShader;
+import kha.graphics4.VertexShader;
+import kha.graphics4.PipelineState;
 import kha.graphics4.IndexBuffer;
 import kha.graphics4.VertexBuffer;
 import kha.graphics4.VertexStructure;
@@ -12,10 +21,18 @@ class Mesh {
     public var vertexBuffer:VertexBuffer;
     public var indexBuffer:IndexBuffer;
 
+    public var pipeline:PipelineState;
+    public var vertexShader:VertexShader;
+    public var fragementShader:FragmentShader;
+
+	public var image:Image;
+    private var textureId:TextureUnit;
+    private var mvpId:ConstantLocation;
+
     public function new() {
     }
 
-    public function updateBuffers() {
+    public function compile() {
         vertexBuffer = new VertexBuffer(Std.int(vertices.length / 3), structure, StaticUsage);
 
 		var vbData = vertexBuffer.lock();
@@ -31,5 +48,30 @@ class Mesh {
 		  ibData[i] = indices[i];
 		}
 		indexBuffer.unlock();
+
+        pipeline = new PipelineState();
+		pipeline.inputLayout = [structure];
+		pipeline.fragmentShader = fragementShader;
+		pipeline.vertexShader = vertexShader;
+		pipeline.depthWrite = true;
+		pipeline.depthMode = CompareMode.Less;
+		// pipeline.cullMode = CullMode.Clockwise; // hide interior hidden faces. Important that verticies are in the right order
+		// pipeline.colorAttachmentCount = 1;
+		// pipeline.colorAttachments[0] = TextureFormat.RGBA32;
+		// pipeline.depthStencilAttachment = DepthStencilFormat.Depth16;
+		pipeline.compile();
+
+		textureId = pipeline.getTextureUnit("myTextureSampler");
+		mvpId = pipeline.getConstantLocation("MVP");
+    }
+
+    public function draw(g:Graphics, mvp:FastMatrix4) 
+    {
+        g.setPipeline(pipeline);
+		g.setTexture(textureId, image);
+        g.setMatrix(mvpId, mvp);
+        g.setVertexBuffer(vertexBuffer);
+        g.setIndexBuffer(indexBuffer);
+        g.drawIndexedVertices();
     }
 }
